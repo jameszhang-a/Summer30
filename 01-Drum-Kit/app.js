6bbb59a92819fd50a0ea6214a0301f7996ed83cc
James Zhang <jameszhanga@gmail.com>
2021-08-10 19:50:41 -0700

    01-Basic function complete

--- 01-Drum-Kit/app.js
+++ 01-Drum-Kit/app.js
@@ -1,4 +1,29 @@
 document.addEventListener('keydown', e => {
   const sound = document.querySelector(`audio[data-key="${e.code}"]`);
-  console.log(sound);
+  const key = document.querySelector(`div[data-key="${e.code}"]`);
+
+  if (!sound) {
+    console.log('not a valid key');
+    return;
+  }
+
+  // Plays note
+  sound.play();
+  sound.currentTime = 0;
+
+  // Gives the styling for a pressed key
+  key.classList.add('playing');
+});
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => {
+  key.addEventListener('transitionend', e => {
+    // A lot of things will transition here (boarder, scale, shadow, ...)
+    // Pick one of them
+    if (e.propertyName === 'transform') {
+      // Removes the styling to return to regular keys
+      key.classList.remove('playing');
+    }
+  });
 });
